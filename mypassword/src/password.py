3a2bd3f9f519703c17a1f319f6667e3ffd85a094
Vicente Gutierrez <vinzegtz@gmail.com>
2020-05-20 20:04:30 -0500

    Create method to generate simple password

--- mypassword/src/password.py
+++ mypassword/src/password.py
@@ -2,4 +2,22 @@ import random
 import string
 
 class Password:
-    pass
+    def __init__(self, length=8):
+        self.length = length
+        self.password = self.__get_randon_string()
+    
+
+    def __str__(self):
+        return f'{self.password}'
+    
+
+    def __get_randon_string(self):
+        random_string = ''
+
+        if self.length < 8:
+            raise Exception('The length cannot be less than 8')
+
+        for i in range(self.length):
+            random_string += random.choice(string.ascii_lowercase)
+        
+        return random_string
