eef3b6465caeb7f383c95e53521f4d1ad09e18c9
Vicente Gutierrez <vinzegtz@gmail.com>
2020-05-22 07:53:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ README = (HERE / 'README.md').read_text()
 setup(
     name = 'mypassword',
     packages = ['mypassword'],
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'Tools for generating strong passwords',
     long_description=README,
     long_description_content_type='text/markdown',
     author = 'Vicente Gutiérrez',
     author_email = 'vinzegtz@gmail.com',
     url = 'https://github.com/vinzegtz/mypassword',
-    download_url = 'https://github.com/vinzegtz/mypassword/archive/v0.0.3-beta.tar.gz',
+    download_url = 'https://github.com/vinzegtz/mypassword/archive/v0.0.4-beta.tar.gz',
     keywords = ['password', 'pass', 'password-manager'],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Security',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3'
     ]
